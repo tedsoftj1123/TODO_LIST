f62ddda04a8d7a28410507b6b72e8ac080e06fa0
이하성 <tedsoftj1123@gmail.com>
2022-07-01 19:54:07 +0900

    [EDIT] Project Structure

---
+++ DmsTodo/src/main/java/com/example/dmstodo/DmsTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmstodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsTodoApplication.class, args);
+	}
+
+}
---
+++ DmsTodo/src/test/java/com/example/dmstodo/DmsTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmstodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
